3cdb11245d0666f52c69732d2d964211047a7efb
=Tatyana Gavrilova <=>
2026-03-24 00:06:16 +0300

    refactoring

--- src/main/java/user/service/UsersApplication.java
+++ src/main/java/user/service/UsersApplication.java
@@ -1,22 +1,13 @@
 package user.service;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class UsersApplication {
 
-    public static void main(String[] args) {
-
-        var configurableApplicationContext = SpringApplication.run(UsersApplication.class, args);
-
-        Flyway.configure()
-                .dataSource(configurableApplicationContext.getBean(DataSource.class))
-                .load()
-                .migrate();
+    static void main(String[] args) {
+        SpringApplication.run(UsersApplication.class, args);
     }
 
 }
